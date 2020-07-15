60a70b3048b5d7f4da5e94e218a8ee588275003c
Lucas Faria <lucas_faria@hst.com.br>
2020-07-15 15:28:41 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(cors({
+    origin: '*'
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+
+    console.log(req.body)
+
+    const response = { status: 'sucesso', resultado: 10 };
+    res.json(response);
+})
+
+app.get('/', (req, res) => {
+    const response = { msg: 'sucesso' };
+    res.json(response);
+})
+
+
+app.listen(3000, function () {
+    console.log('servidor conectado port 3000')
+});
