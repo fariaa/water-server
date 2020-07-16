3e75ed360860f9609e25fc7b203e38e77058c64c
Lucas Faria <lucas_faria@hst.com.br>
2020-07-15 21:50:06 -0300

    ajustes para aceitar porta dinamicamente

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,6 +24,6 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(3000, function () {
-    console.log('servidor conectado port 3000')
+app.listen(PORT, function () {
+    console.log(`servidor conectado port ${PORT}`)
 });
